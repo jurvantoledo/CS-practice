ae66a9551dbfb3deafdf831ceb00ad03f6119c0d
Jur van Toledo <jurvantoledo@live.nl>
2024-11-22 10:29:09 +0100

    Update LongestCommonPrefix.cs

--- LongestCommonPrefix/LongestCommonPrefix.cs
+++ LongestCommonPrefix/LongestCommonPrefix.cs
@@ -2,8 +2,19 @@ using System;
 
 public class Solution {
     public string LongestCommonPrefix(string[] strs) {
-        Console.WriteLine(string.Join(", ", strs)); // This will print the array elements
-        return ""; // Placeholder return
+        if (strs == null || strs.Length == 0)
+            return "";
+
+        string prefix = strs[0];
+
+        for (int i = 1; i < strs.Length; i++) {
+            while (strs[i].IndexOf(prefix) != 0) {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+                if (prefix == "")
+                    return "";
+            }
+        }
+        return prefix;
     }
     
     static void Main() {
