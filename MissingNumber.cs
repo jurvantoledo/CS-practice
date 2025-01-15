23b014e6d8f36956bb49d89cce908fb173b60242
Jur van Toledo <jurvantoledo@live.nl>
2025-01-15 09:13:06 +0100

    Create MissingNumber.cs

---
+++ MissingNumber.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Solution {
+    public int MissingNumber(int[] nums) {
+        int[] arr = new int[nums.Length + 1];
+        Array.Sort(nums);
+        int res = 0;
+        
+        
+        for (int i = 0; i < nums.Length + 1; i++) {
+            arr[i] = i;
+        }
+        
+        
+        for (int i = 0; i < arr.Length; i++) {
+            if (i >= nums.Length || nums[i] != arr[i]) {
+                return arr[i];
+            }
+        }
+        
+        return 0;
+    }
+    
+    static void Main() {
+        Solution sol = new Solution();
+        int[] nums = new int[] {1, 2};
+        Console.WriteLine($"{sol.MissingNumber(nums)}");
+    }
+}
